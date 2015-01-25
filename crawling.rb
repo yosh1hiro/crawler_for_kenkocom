50889ceeded75220b4fca9da1533d23b3b3d87f2
yoshihiro asada <yoshihiro0921@gmail.com>
2015-01-26 07:25:53 +0900

    modify scraping
    ケンコーコムの必要情報をとるためのxpathを設置

--- crawling.rb
+++ crawling.rb
@@ -2,33 +2,50 @@
 
 require 'anemone'
 require 'mongo'
+require 'nokogiri'
+require 'kconv'
 require 'bundler/setup'
 
 options = {
   :user_agent => "AnemoneCrawler/0.0.1",
   :storage => Anemone::Storage.MongoDB,
-#  :delay => 1,
-  :depth_limit => 1,
+  :delay => 1,
+  :depth_limit => 0,
   :skip_query_strings => true
 }
 
 
 # クロールの起点となるURLを指定
-urls = ["http://www.kenko.com/ranking/01/"]
-Anemone.crawl(urls, options) do |anemone|
+# urls = ["http://www.kenko.com/ranking/01/"]
+Anemone.crawl("http://www.kenko.com/product/item/itm_7721527072.html", options) do |anemone|
 
 	# 巡回先の絞り込み
 	anemone.focus_crawl do |page|
 		page.links.keep_if { |link|
-			link.to_s.match(/product/)
+			link.to_s.match(/\/product\/item\//)
 		}
 	end
 
 	# 取得したページに対する処理
 	anemone.on_every_page do |page|
-		 
-		puts page.url.to_s
-		puts page.doc.xpath("//head/title/text()").first.to_s if page.doc
-	
+
+
+		name = page.doc.xpath('//*[@id="prodFeatures"]/h1').text
+
+		price = page.doc.search('//*/li[@class="price"]/span[1]').text
+
+		# 後で正規表現で数字だけ抽出する必要あり
+		jan_code = page.doc.xpath('//*/div[@itemprop="identifier"]').text
+
+		description = page.doc.xpath('//*/div[@class="description"]/p').text
+		
+ 
+		puts name
+		puts price
+		puts jan_code
+		puts description
+
+
+		puts page.url.to_s	
 	end
 end
